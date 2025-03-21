c4393d7562c9ef9333e637f5b375e1a021faef3b
rishi-awtde18 <98142802+rishi-awtde18@users.noreply.github.com>
2025-03-21 08:03:41 +0000

    Conversion of DataTypes

---
+++ 01_Basics/01_Conversions.js
@@ -0,0 +1,25 @@
+let age=true;
+let isValid=0;
+
+//let convertToString=String(age);
+let convertToNumber=Number(age);//"33"
+//let convertToBoolean =Boolean(isValid);
+//console.log(age);
+console.log(convertToNumber);
+console.log(typeof convertToString);
+console.log(typeof convertToNumber);
+console.log(typeof convertToBoolean);
+
+//Convert To Number
+//"33" => 33
+//"33abc"=> NaN
+//true=> 1; false=> 0
+
+//Convert To Boolean
+// 1=> true; 0=> false;
+// "" => false; 
+//"Rishi"=> true;
+
+
+
+
