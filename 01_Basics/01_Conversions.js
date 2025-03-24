fea44dfd9c21ffff6baa192e95d712db92680cb6
rishi-awtde18 <98142802+rishi-awtde18@users.noreply.github.com>
2025-03-24 19:22:47 +0000

    DataType_Summary

--- 01_Basics/01_Conversions.js
+++ 01_Basics/01_Conversions.js
@@ -1,14 +1,14 @@
-let age=true;
-let isValid=0;
+//let age=true;
+//let isValid=0;
 
 //let convertToString=String(age);
-let convertToNumber=Number(age);//"33"
+//let convertToNumber=Number(age);//"33"
 //let convertToBoolean =Boolean(isValid);
 //console.log(age);
-console.log(convertToNumber);
-console.log(typeof convertToString);
-console.log(typeof convertToNumber);
-console.log(typeof convertToBoolean);
+//console.log(convertToNumber);
+//console.log(typeof convertToString);
+//console.log(typeof convertToNumber);
+//console.log(typeof convertToBoolean);
 
 //Convert To Number
 //"33" => 33
@@ -19,6 +19,20 @@ console.log(typeof convertToBoolean);
 // 1=> true; 0=> false;
 // "" => false; 
 //"Rishi"=> true;
+
+//===============OPERATORS=================//
+let a=10;
+let str1="Rishi";
+let str2="Awatade";
+let str3= str1+str2;
+//let str4= str1.concat(str2);
+//console.log(str3);
+//console.log(++a);
+//console.log(a--);
+//console.log(2**3);
+console.log("2"+1+2);
+console.log(1+2+"4");
+
 
 
 
---
+++ 01_Basics/01_dataTypes-Summary.js
@@ -0,0 +1,52 @@
+//================Primitive (Call By Value)=================//
+//There are 7 types in Javascript
+//String,Number,Boolean,null,undefined,BigInt,Symbol
+//e.g
+const num=10;//number
+let str="Rishi";//string
+let isValid=true;//Boolean
+let phoneNum=null;//null
+let email;//undefined
+let bigInt=12345678910545655n//BigInt
+let uniqNum=Symbol("123");//Symbol
+
+//Note:
+//Primitive dataTypes are stored in Stack memory.
+//e.g
+let userName="Rishi";
+let firstName=userName;//Giving only copy of userName from stack memory
+firstName="Rushikesh"
+console.log(firstName);
+
+//=======================================================//
+//=============Non-Primitive (Call By Reference)========//
+//There are 3 types in Javascript.
+//Array, Object, Functions
+//e.g
+let arr = ["Rishi","Mangala","Gajanan","Awatade"];//Array
+
+//Object
+let myObj={
+    firstName:"Rishi",
+    lastName: "Awatade",
+    age:28,
+}
+//--
+
+//Functions
+let myFun=function()
+{
+    return 0;
+}
+//--
+
+//Note:
+//Primitive dataTypes are stored in Heap memory.
+//e.g
+let newObj=myObj;
+newObj.firstName="Rushikesh";//Direct change to the object which is located in Heap Memory.
+console.log(myObj);
+
+//====================================================================//
+
+
