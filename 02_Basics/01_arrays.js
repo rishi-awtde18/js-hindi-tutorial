5d8024a1586c0ca57a4bdb69625128044d18c0a4
rishi-awtde18 <98142802+rishi-awtde18@users.noreply.github.com>
2025-06-08 19:08:48 +0000

    arrays tutorials

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,24 @@
+const myArr= [12,13,14,15,16];
+console.log(myArr[2]);
+
+const myNewArr = new Array(10,"Rishi",false); //Stored all datatypes in a single array
+
+const myArr1=[12,45,"Rishi",false];
+console.log(myArr1[2]);
+
+//Arrays Methods
+
+//myArr.push(6);
+//console.log(myArr);
+
+myArr.pop();
+//console.log(myArr);
+
+myArr.unshift(9);
+//console.log(myArr);
+
+myArr.shift();
+//console.log(myArr);
+
+console.log(myArr.includes(2));
+console.log(myArr);
